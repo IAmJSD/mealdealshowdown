9b4fa15e786854f36b1819bc69c1af9517c05459
Astrid Gealer <astrid@gealer.email>
2025-03-02 00:31:14 +0000

    padding

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -2,7 +2,7 @@
 
 export default function MainContainer({ children }: { children: React.ReactNode }) {
     return (
-        <main className="max-w-4xl mx-auto mt-16">
+        <main className="max-w-4xl mx-auto mt-16 px-8 lg:px-0">
             {children}
         </main>
     );
